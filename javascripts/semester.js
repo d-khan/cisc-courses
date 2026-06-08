c89a3d5ca29b4131026d59da541369fd95648e1a
Danish Khan <dkhan1010@gmail.com>
2026-06-08 13:28:01 -0700

    Deployed 820739a with MkDocs version: 1.6.1

---
+++ javascripts/semester.js
@@ -0,0 +1,13 @@
+// semester.js
+
+window.semesterConfig = {
+        // make sure the format is yyyy-mm-dd and in string format, enclosed in double quotes
+        semesterStart: "2026-06-15",
+
+        semesterEnd: "2026-08-08",
+
+        // When go LIVE, change today to null
+         today: null
+
+        //today: "2026-08-10"
+};
